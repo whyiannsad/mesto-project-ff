5ab21858a95e5215905ff004231a54ea86989e74
Ivan Znatnov <znatnov.ivan@gmail.com>
2023-12-22 14:18:46 +0300

    final fix

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,8 @@ const cardList = document.querySelector('.places__list');
 function createElement(cardName, cardLink, deleteCard) {
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
     const cardImage = cardElement.querySelector('.card__image');
-	const cardTitle = cardElement.querySelector('.card__title');
-	const deleteButton = cardElement.querySelector('.card__delete-button');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
     cardImage.src = cardLink;
     cardImage.alt = cardName;
     cardTitle.textContent = cardName;
@@ -18,9 +18,9 @@ function createElement(cardName, cardLink, deleteCard) {
 }
 // @todo: Функция удаления карточки
 function deleteCard (card) {
-    card.remove();
-};
+card.remove();
+}
 // @todo: Вывести карточки на страницу
 initialCards.forEach( element => {
 	cardList.append(createElement(element.name, element.link, deleteCard));
-});
+})

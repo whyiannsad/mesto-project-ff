6fa4338d6a62d10a18791b1517ae4c4623da5f24
Ivan Znatnov <znatnov.ivan@gmail.com>
2023-12-22 12:20:16 +0300

    m

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const cardList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-function createElement(cardName, cardLink, deleteBut) {
+function createElement(cardName, cardLink, deleteCard) {
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
     const cardImage = cardElement.querySelector('.card__image');
 	const cardTitle = cardElement.querySelector('.card__title');

3d1af89ad6176e4b6656498e5119b50ffb8606f8
Ivan Znatnov <znatnov.ivan@gmail.com>
2023-12-22 11:43:20 +0300

    making function of deleting cards

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/whyiannsad/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const cardList = cardContainer.querySelector('.places__list');
 // @todo: Функция создания карточки
-
+function addElement(cardName, cardLink, deleteButton) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+	const cardTitle = cardElement.querySelector('.card__title');
+	const deleteButton = cardElement.querySelector('.card__delete-button');
+    cardImage.src = cardLink;
+    cardImage.alt = cardName;
+    cardTitle.textContent = cardName;
+    deleteButton.addEventListener('click', function() {
+        deleteButton(card);
+    })
+    return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(card) {
+	card.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+    places.append(addElement(item.name, item.link, deleteCard));
+});
